baaa5e5e089c1cc8fca5ee4ea0301bdd8cbaacd3
Rahul Muttineni <rahulmutt@gmail.com>
2018-01-21 17:50:16 +0530

    First

---
+++ src/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Servant
+import Servant.Server
+import Data.Aeson
+import Network.Wai.Servlet.Handler.Jetty
+
+type API = "fib" :> Capture "n" Int :> Get '[JSON] Value
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+api :: Proxy API
+api = Proxy
+
+server :: Application
+server = serve api fibHandler
+  where fibHandler maybeN
+          | Just n <- maybeN, n > 0
+          = return $ object ["n" .= n, "fib" .= fib n]
+          | otherwise = throwError $ err412 { errBody = "Precondition Failed: n > 0" }
+
+main :: IO ()
+main = run 9000 server
